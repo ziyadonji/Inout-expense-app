8fb8b4c160a7bd2126acc8847cd121c3154cf5cf
ziyadonji <ziyadonji@gmail.com>
2020-07-13 13:12:24 +0530

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,17 +8,65 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "INOUT",
-    home: Home(),
-      
+    return MaterialApp(
+      title: "INOUT",
+      home: Home(),
     );
   }
 }
+
 class Home extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      cost: 100,
+      date: DateTime.now(),
+      id: "1",
+      title: "petrol",
+    ),
+    Transaction(
+      cost: 50,
+      date: DateTime.now(),
+      id: "2",
+      title: "book",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('InOut'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[700],
+      ),
+      body: Column(children: [
+        Container(
+          child: Card(
+            child: Center(child: Text("here is chart")),
+          ),
+          width: double.infinity,
+          height: 150,
+          margin: EdgeInsets.all(20),
+        ),
+        Column(
+          children:transaction.map((e) {
+            return Card(
+              child: Row(
+                children:[
+                  Container(
+                    child: Text(e.cost.toString()),
+                  ),
+                  Column(children: <Widget>[
+                    Text(e.title),
+                    Text(e.date.toString()),
+                  ],)
+                ]
+              ),
+            );
+
+          }).toList()
+        )
+      ]),
     );
   }
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+
+
+class Transaction {
+  final int cost;
+  final String title;
+  final String id;
+  final DateTime date;
+
+  Transaction({
+    @required this.cost,
+    @required this.date,
+    @required this.id,
+    @required this.title,
+  });
+}
